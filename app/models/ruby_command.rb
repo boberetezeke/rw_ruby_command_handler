65c712dcab5a1800c7875a402e23f1f00f563396
Steve Tuckner <steve@steve-laptop.(none)>
2009-11-15 00:36:26 -0600

    fixed to accomodate new way acts_as_database_object is done

--- app/models/ruby_command.rb
+++ app/models/ruby_command.rb
@@ -1,13 +1,14 @@
 class RubyCommand < ActiveRecord::Base
-  acts_as_database_object
+  acts_as_database_object do
 
-  has_field :command
-  has_field :result, :type => "text", :control_type => "partial", 
-            :display_partial => "command_results"
-  has_field :output, :type => "text", :control_type => "partial",
-            :display_partial => "command_output"
+    has_field :command
+    has_field :result, :type => "text", :control_type => "partial", 
+              :display_partial => "command_results"
+    has_field :output, :type => "text", :control_type => "partial",
+              :display_partial => "command_output"
 
-  has_field :when_created, :type => :datetime
+    has_field :when_created, :type => :datetime
+  end
 
   link_text {|x,c| if x.command.size > 20 then; x.command[0..17] + "..."; else; x.command; end}
 end
